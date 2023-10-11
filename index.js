9ae34afb3fa06fb86414d0c0952570e36f0b8d5b
Shubhansu kumar <shubhansu2021@gmail.com>
2023-10-11 12:47:46 +0530

    Final Setup: Good to go
    Signed-off-by: Shubhansu kumar <shubhansu2021@gmail.com>

--- index.js
+++ index.js
@@ -5,36 +5,28 @@ async function updateReadme() {
     fs.readFile('quotes.txt', (err, data) => {
         if (err) throw err;
         prevData += data.toString();
-        // console.log(data.toString());
     });
-
-    // fetch("https://api.quotable.io/random").then((v) => {
-
-    // }).catch((e) => {
-
-    // }).then(() => {});
-
-    let data = {};
+    
+    let readme = "";
+    
     try {
-
         const response = await fetch("https://api.quotable.io/random");
-        data = await response.json();
+        const data = await response.json();
+
+        readme = `Quote on ${new Date().toUTCString()}: `;
+    
+        if (response.ok) {
+            readme += ` ${data["content"]} - ${data["author"]}`;
+        } else {
+            throw new Error('response not OK');
+        }
     } catch (e) {
-        console.log(e);
-        return;
-    }
-
-    let readme = `Quote on ${new Date().toUTCString()}: `;
-
-    if (response.ok) {
-        readme += ` ${data["content"]} - ${data["author"]}`;
-    } else {
         readme += `Fetching failed`;
     }
-
+    
     prevData += "\n";
     prevData += readme;
-
+    
     console.log(prevData);
 }
 
